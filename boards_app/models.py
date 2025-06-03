4afa7b1f9a4c239f3508a1ca7374d7e1b954d9a5
JonasMahlburg <darrylmanga@yahoo.com>
2025-06-03 10:47:04 +0200

    feat : Tasks with status 'to-do' will be counted in Boards in 'tasks_to_do_count'

--- boards_app/models.py
+++ boards_app/models.py
@@ -6,7 +6,11 @@ from user_auth_app.models import User
 class Boards(models.Model):
     title = models.CharField(max_length=255)
     member_count = models.ManyToManyField(User, related_name='boards_member_count')
-    tasks_to_do_count = models.ManyToManyField(Tasks, related_name='boards_with_this_task')
+    tasks_to_do_count = models.ManyToManyField(
+        Tasks,
+        related_name='boards_with_this_task',
+        limit_choices_to={'status': 'to-do'}
+    )
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -21,7 +25,10 @@ class Boards(models.Model):
     class Meta:
         verbose_name = 'Board'
         verbose_name_plural = 'Boards'
-
+   
+    @property
+    def tasks_to_do_count(self):
+        return self.tasks.filter(status='to-do').count()
+    
     def __str__(self):
         return self.title
-
--- tasks_app/models.py
+++ tasks_app/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 # Create your models here.
 class Tasks(models.Model):
+    STATUS_CHOICES = [
+        ('to-do', 'To Do'),
+        ('in-progress', 'In Progress'),
+        ('reviewing', 'Reviewing'),
+        ('done', 'Done'),
+    ]
+
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=150)
     date = models.DateTimeField(auto_now=True)
@@ -10,6 +17,11 @@ class Tasks(models.Model):
     prio = models.CharField(max_length=10, help_text="please use: 'critical', 'high', 'medium' or 'low'")
     worked = models.CharField(max_length=100)
     board = models.ForeignKey('boards_app.Boards', on_delete=models.CASCADE, related_name='tasks'
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='to-do'
     )
     
     class Meta:
