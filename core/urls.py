e06884402fe010be49cf9702bf6ba11d67cea927
JonasTheOtter <darrylmanga@yahoo.com>
2025-07-21 10:33:30 +0000

    made app ready for server

--- core/urls.py
+++ core/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from core import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('tasks_app.api.urls')),
     path('api/', include('boards_app.api.urls')),
     path('api/', include('user_auth_app.api.urls')),
-]
+] + staticfiles_urlpatterns()
