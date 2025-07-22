cc8096c7f02b3634d533a73433ff1e597a378011
JonasMahlburg <darrylmanga@yahoo.com>
2025-07-22 15:54:58 +0200

    fix update admin panel at tasks_app

--- tasks_app/admin.py
+++ tasks_app/admin.py
@@ -7,7 +7,7 @@ class TasksAdmin(admin.ModelAdmin):
         (
             None,
             {
-                "fields": ["title", "description", "due_date", "priority"],
+                "fields": ['id', "title", "description", "due_date", "priority"],
             },
         ),
         (
