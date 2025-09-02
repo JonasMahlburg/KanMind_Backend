4a6cbb5c0727a180e241fa15dbb4b99404df2265
JonasMahlburg <darrylmanga@yahoo.com>
2025-09-02 11:59:31 +0200

    update settings.py

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "$*"
 git push
-ssh paintingotter34@34.12.5.36 "cd projects/KanMind_Backend && sudo git pull && sudo supervisorctl restart kanmind-gunicorn"
+ssh paintingotter34@34.12.5.36 "cd projects/KanMind_Backend && source .env/bin/activate && git pull && sudo supervisorctl restart kanmind-gunicorn"
